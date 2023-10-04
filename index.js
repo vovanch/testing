3066b9e212f3f2dbafe224a6e25b1e51ad87b3ba
Volodymyr <vvoitovych1@gmail.com>
2023-10-04 21:15:24 +0300

    Loops

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
 let personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -6,8 +8,30 @@ let personalMovieDB = {
 	genres: [],
 	privat: false
 };
-let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+let lastFilm, rateFilm;
+for (let i = 0; i < numberOfFilms; i++) {
+	
+	lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
 	rateFilm = +prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies["lastFilm"] = rateFilm;
+	if (lastFilm != "" && rateFilm != "" && lastFilm !=null && rateFilm != null && lastFilm.length < 10 && rateFilm <5) {
+		personalMovieDB.movies[lastFilm] = rateFilm;
+	}
+	else {
+		i--; }
+}
+if (personalMovieDB.count < 10) {
+	alert("Просмотрено довольно мало фильмов");
+} 
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30)
+{
+	alert("Вы классический зритель.");
+}
+else if (personalMovieDB.count >= 30)
+{
+	alert("Вы киноман");
+}
+else {
+	alert("Произошла ошибка");
+}
 console.log(personalMovieDB);
+
