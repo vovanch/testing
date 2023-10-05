d15c11ee8a7c9c2e38aaec3c8834a80c93acce93
Volodymyr <vvoitovych1@gmail.com>
2023-10-05 22:44:34 +0300

    new tasks

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let numberOfFilms;
-numberOfFilms = +prompt("Скільки фільмів ви вже передивились?", "");
-
+function start() {
+	numberOfFilms = +prompt("Скільки фільмів ви вже передивились?", "");
+	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt("Скільки фільмів ви вже передивились?", "");
+	}
+}
+start();
 let personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -9,34 +14,52 @@ let personalMovieDB = {
 	privat: false
 };
 let lastFilm, rateFilm;
-//for (let i = 0; i < numberOfFilms; i++) {
-let i=0;
-do {
-		
-	
-	lastFilm = prompt("Один із останніх переглянутих фільмів?", ""),
-	rateFilm = +prompt("На скільки оціните його?", "");
-	if (lastFilm != "" && rateFilm != "" && lastFilm !=null && rateFilm != null && lastFilm.length < 10 && rateFilm <5) {
-		personalMovieDB.movies[lastFilm] = rateFilm;
+
+function showMyDB (hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+showMyDB(personalMovieDB.privat);
+function rememberMyFilms() {
+	let i=0;
+	do {
+		lastFilm = prompt("Один із останніх переглянутих фільмів?", ""),
+		rateFilm = +prompt("На скільки оціните його?", "");
+		if (lastFilm != "" && rateFilm != "" && lastFilm !=null && rateFilm != null && lastFilm.length < 10 && rateFilm <5) {
+			personalMovieDB.movies[lastFilm] = rateFilm;
+		}
+		else {
+			i--; }
+		i++;
+	}
+	while (i<numberOfFilms);
+}
+rememberMyFilms();
+
+const detectPersonalLevel = () => {
+	if (personalMovieDB.count < 10) {
+		alert("Переглянуто доволі мало фільмів!");
+	} 
+	else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30)
+	{
+		alert("Ви класичний глядач.");
+	}
+	else if (personalMovieDB.count >= 30)
+	{
+		alert("Ви кіноман!!!");
 	}
 	else {
-		i--; }
-	i++;
+		alert("Виникла помилка!");
+	}
+};
+writeYourGenres();
+detectPersonalLevel();
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {	 
+		personalMovieDB.genres[i-1] = prompt(`Який ваш улюблений жанр фільмів під номером ${i}?`, "");
+	}
 }
-while (i<numberOfFilms);
-if (personalMovieDB.count < 10) {
-	alert("Переглянуто доволі мало фільмів!");
-} 
-else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30)
-{
-	alert("Ви класичний глядач.");
-}
-else if (personalMovieDB.count >= 30)
-{
-	alert("Ви кіноман!!!");
-}
-else {
-	alert("Виникла помилка!");
-}
+
 console.log(personalMovieDB);
 
